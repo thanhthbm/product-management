75c89784712d9c795925b1db21031a6b2250d66d
Vũ Minh Thành <thanhthbm@gmail.com>
2025-03-22 19:20:58 +0700

    image preview when uploading image

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -137,5 +137,29 @@ if (showAlert){
     });
 }
 
+//upload image
+const uploadImage = document.querySelector('[upload-image]');
+if (uploadImage){
+    const uploadImageInput = uploadImage.querySelector('[upload-image-input]');
+    const uploadImagePreview = uploadImage.querySelector('[upload-image-preview]');
+
+    uploadImageInput.addEventListener('change', (e) => {
+       const file = e.target.files[0];
+       if (file){
+           uploadImagePreview.src = URL.createObjectURL(file);
+           const hideBtn = uploadImage.querySelector('[close-button]');
+           if (uploadImagePreview.src){
+               hideBtn.classList.remove('hide-btn');
+           }
+           hideBtn.addEventListener('click', () => {
+               uploadImagePreview.src = '';
+               uploadImageInput.value = '';
+           });
+       }
+    });
+
+
+}
+
 
 
